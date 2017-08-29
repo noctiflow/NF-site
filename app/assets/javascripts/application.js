36886b973ec56226035c6352c0e2a683288b80b2
Richard Wang <rygoon@gmail.com>
2017-08-29 14:52:17 -0700

    fixed navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,24 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+// NAVBAR JQUERY========================
+
+$(document).ready(function() {
+  function checkScroll(){
+      var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+
+      if($(window).scrollTop() > startY){
+          $('.navbar').addClass("scrolled");
+      }else{
+          $('.navbar').removeClass("scrolled");
+      }
+  }
+
+  if($('.navbar').length > 0){
+      $(window).on("scroll load resize", function(){
+          checkScroll();
+      });
+  }
+})
