0958114fed08c7f5ef9ca991520df403df358eff
dextrizzle <alfonzo_01@hotmail.com>
2017-09-08 16:53:26 -0700

    add in localization

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,10 @@ module Company
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.i18n.available_locales = [:en, :zh-TW]
+    config.i18n.available_locales = [:en, :zh_TW]
     config.i18n.default_locale = :en
+    config.i18n.fallbacks = true
+
 
   end
 end
