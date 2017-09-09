7924101ff3dc72b07cefdafba9273891b462c16b
dextrizzle <alfonzo_01@hotmail.com>
2017-09-08 17:20:16 -0700

    persistent locale setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
-  get 'portfolio' => 'portfolio#index'
-  get 'pricing' => 'pricing#index'
+  scope "(:locale)", locale: /en|zh_TW/ do
+    root 'home#index'
+    get 'portfolio' => 'portfolio#index'
+    get 'pricing' => 'pricing#index'
 
-  match '/contacts',     to: 'contacts#new',             via: 'get'
-  resources "contacts", only: [:new, :create]
+    match '/contacts',     to: 'contacts#new',             via: 'get'
+    resources "contacts", only: [:new, :create]
 
-  get 'marketing' => 'marketing#index'
-  get 'team' => 'team#index'
+    get 'marketing' => 'marketing#index'
+    get 'team' => 'team#index'
+  end
 end
