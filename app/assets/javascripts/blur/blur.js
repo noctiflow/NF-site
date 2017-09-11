12acf556e614637327c13a42e1760f9081892122
Richard Wang <rygoon@gmail.com>
2017-09-10 22:54:23 -0700

    added chinese translation to pages

--- app/assets/javascripts/blur/blur.js
+++ app/assets/javascripts/blur/blur.js
@@ -28,3 +28,69 @@ $(function() {
       });
 
     });
+
+//==============================================================================
+
+$(function() {
+
+      var $container	= $('#ib-container2'),
+        $articles	= $container.children('article'),
+        timeout;
+
+      $articles.on( 'mouseenter', function( event ) {
+
+        var $article	= $(this);
+        clearTimeout( timeout );
+        timeout = setTimeout( function() {
+
+          if( $article.hasClass('active') ) return false;
+
+          $articles.not( $article.removeClass('blur').addClass('active') )
+               .removeClass('active')
+               .addClass('blur');
+
+        }, 65 );
+
+      });
+
+      $container.on( 'mouseleave', function( event ) {
+
+        clearTimeout( timeout );
+        $articles.removeClass('active blur');
+
+      });
+
+    });
+
+// =============================================================================
+
+$(function() {
+
+      var $container	= $('#ib-container3'),
+        $articles	= $container.children('article'),
+        timeout;
+
+      $articles.on( 'mouseenter', function( event ) {
+
+        var $article	= $(this);
+        clearTimeout( timeout );
+        timeout = setTimeout( function() {
+
+          if( $article.hasClass('active') ) return false;
+
+          $articles.not( $article.removeClass('blur').addClass('active') )
+               .removeClass('active')
+               .addClass('blur');
+
+        }, 65 );
+
+      });
+
+      $container.on( 'mouseleave', function( event ) {
+
+        clearTimeout( timeout );
+        $articles.removeClass('active blur');
+
+      });
+
+    });
